c0389fc22b35456b5f2205c77adfc4613f150f0e
Juliano Firme <julianofirme23@gmail.com>
2024-08-28 11:45:02 -0300

    test: health check route

---
+++ src/tests/health-check.test.ts
@@ -0,0 +1,12 @@
+import { test, expect } from 'vitest'
+import app from './helpers/fastify.js'
+
+test.only('GET /healthcheck should return status OK', async () => {
+  const response = await app.inject({
+    method: 'GET',
+    url: '/healthcheck',
+  })
+
+  expect(response.statusCode).toBe(200)
+  expect(response.json()).toEqual({ status: 'ok' })
+})

ad6865d06465ab7a1de0208e84a6c84e53e81d62
Juliano Firme <julianofirme23@gmail.com>
2024-08-29 17:20:00 -0300

    feat: graceful shutdown

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import app from './app.js'
+import process from 'process'
+import { logger } from './logger/logger.js'
+
 const PORT = 3000
 
 async function main() {
   try {
-    await app.listen({
+    app.listen({
       port: PORT,
       host: '0.0.0.0',
     })
@@ -11,6 +15,20 @@ async function main() {
     app.log.error(e)
     process.exit(1)
   }
+
+  const shutdown = async () => {
+    logger.info('Shutting down gracefully...')
+    try {
+      await app.close()
+      logger.info('Server closed')
+    } catch (err) {
+      logger.error('Error during shutdown', err)
+    }
+    process.exit(0)
+  }
+
+  process.on('SIGTERM', shutdown)
+  process.on('SIGINT', shutdown)
 }
 
 main()

4680f1b8d094ffb5e00a4ab321b8158b9727670e
Juliano Firme <julianofirme23@gmail.com>
2024-08-29 17:25:18 -0300

    fix: reset the investment table in the integration test

--- src/tests/helpers/reset-db.ts
+++ src/tests/helpers/reset-db.ts
@@ -4,6 +4,7 @@ const prisma = new PrismaClient()
 
 export default async () => {
   await prisma.$transaction([
+    prisma.investment.deleteMany(),
     prisma.deposit.deleteMany(),
     prisma.wallet.deleteMany(),
     prisma.user.deleteMany(),
